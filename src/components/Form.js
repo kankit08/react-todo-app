0c37045db4bd2e77a1880dc957c8b9af822d1096
kankit08 <kankit08@gmail.com>
2023-02-28 19:53:04 +0530

    disable btn and toastify added

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,26 +1,41 @@
-import React from 'react'
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer, toast } from "react-toastify";
 
-const Form = ({inputText, setInputText,todos,setTodos,setStatus}) => {
-
+const Form = ({ inputText, setInputText, todos, setTodos, setStatus }) => {
   const inputTextHandler = (e) => {
     setInputText(e.target.value);
-  }
+  };
 
   const submitTodoHandler = (e) => {
-    e.preventDefault()
-    setTodos([...todos, {text: inputText, completed: false, id: Math.random()*1000}])
-    setInputText("")
-  }
+    e.preventDefault();
+    setTodos([
+      ...todos,
+      { text: inputText, completed: false, id: Math.random() * 1000 },
+    ]);
+
+    setInputText("");
+  };
 
   const statusHandler = (e) => {
     setStatus(e.target.value);
-  }
-    return (
-        <form>
-      <input value={inputText} onChange={inputTextHandler} type="text" className="todo-input" />
-      <button onClick={submitTodoHandler} className="todo-button" type="submit">
+  };
+  return (
+    <form>
+      <input
+        value={inputText}
+        onChange={inputTextHandler}
+        type="text"
+        className="todo-input"
+      />
+      <button
+        onClick={submitTodoHandler}
+        disabled={!inputText}
+        className="todo-button"
+        type="submit"
+      >
         <i className="fas fa-plus-square"></i>
       </button>
+      <ToastContainer />
       <div className="select">
         <select onChange={statusHandler} name="todos" className="filter-todo">
           <option value="all">All</option>
@@ -29,7 +44,7 @@ const Form = ({inputText, setInputText,todos,setTodos,setStatus}) => {
         </select>
       </div>
     </form>
-    )
-}
+  );
+};
 
-export default Form
+export default Form;
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,35 +1,48 @@
-import React from 'react'
+import React from "react";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer, toast } from "react-toastify";
 
-const Todo = ({text, todos, todo, setTodos}) => {
+const Todo = ({ text, todos, todo, setTodos }) => {
+  const toastMessage = () => {
+    toast.success("TODO task deleted successfully", {
+      position: toast.POSITION.TOP_CENTER,
+    });
+  };
 
-    const deleteHandler = () => {
-        setTodos(todos.filter( (elm) => elm.id !== todo.id ))
-    }
+  const deleteHandler = () => {
+    setTodos(todos.filter((elm) => elm.id !== todo.id));
+    toastMessage();
+    <ToastContainer />;
+  };
 
-    const completeHandler = () => {
-        setTodos(todos.map(item => {
-            if(item.id === todo.id){
-                return{
-                    ...item, completed: !item.completed
-                }
-            }
-            return item
-        }))
-    }
+  const completeHandler = () => {
+    setTodos(
+      todos.map((item) => {
+        if (item.id === todo.id) {
+          return {
+            ...item,
+            completed: !item.completed,
+          };
+        }
+        return item;
+      })
+    );
+  };
 
-    return (
-        <div className="todo">
-            <li className={`todo-item ${todo.completed ? "completed" : ''} `}>{text}</li>
-            <button onClick={completeHandler} className="complete-btn">
-                <i className="fas fa-check"></i>
-            </button>
+  return (
+    <div className="todo">
+      <li className={`todo-item ${todo.completed ? "completed" : ""} `}>
+        {text}
+      </li>
+      <button onClick={completeHandler} className="complete-btn">
+        <i className="fas fa-check"></i>
+      </button>
 
-            <button onClick={deleteHandler} className="complete-btn">
-                <i className="fas fa-trash">
-            </i></button>
-           
-        </div>
-    )
-}
+      <button onClick={deleteHandler} className="complete-btn">
+        <i className="fas fa-trash"></i>
+      </button>
+    </div>
+  );
+};
 
-export default Todo
+export default Todo;
