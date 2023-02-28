89cf4c1c375158972066fe1b3aebace822e75585
kankit08 <kankit08@gmail.com>
2023-02-28 19:56:28 +0530

    title updated

--- src/App.js
+++ src/App.js
@@ -1,70 +1,70 @@
-import React, { useState, useEffect } from 'react'
-import './App.css';
-import Form from './components/Form'
-import TodoList from './components/TodoList'
-
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
 
 const App = () => {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
 
-  const [inputText, setInputText] = useState("")
-  const [todos, setTodos] = useState([])
-  const [status, setStatus] = useState('all')
-  const [filteredTodos, setFilteredTodos] = useState([])
+  useEffect(() => {
+    getLocalTodos();
+  }, []);
 
-  useEffect( () => {
-    getLocalTodos()
-  }, [])
-
-  useEffect( () => {
-    filterHandler()
-    saveLocalTodos()
+  useEffect(() => {
+    filterHandler();
+    saveLocalTodos();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [todos, status])
+  }, [todos, status]);
 
   const filterHandler = () => {
-    switch(status){
-      case 'completed':
-        setFilteredTodos(todos.filter(todo => todo.completed === true))
-        break
-      case 'uncompleted':
-        setFilteredTodos(todos.filter(todo => todo.completed === false))
-        break
+    switch (status) {
+      case "completed":
+        setFilteredTodos(todos.filter((todo) => todo.completed === true));
+        break;
+      case "uncompleted":
+        setFilteredTodos(todos.filter((todo) => todo.completed === false));
+        break;
       default:
-        setFilteredTodos(todos)
-        break
+        setFilteredTodos(todos);
+        break;
     }
-  }
+  };
 
   const saveLocalTodos = () => {
-   localStorage.setItem('todos', JSON.stringify(todos))
-    }
+    localStorage.setItem("todos", JSON.stringify(todos));
+  };
 
   const getLocalTodos = () => {
-    if(localStorage.getItem('todos') === null){
-      localStorage.setItem('todos', JSON.stringify([]))
-    }else{
-     let todoLocal=  JSON.parse(localStorage.getItem("todos"))
-     setTodos(todoLocal)
+    if (localStorage.getItem("todos") === null) {
+      localStorage.setItem("todos", JSON.stringify([]));
+    } else {
+      let todoLocal = JSON.parse(localStorage.getItem("todos"));
+      setTodos(todoLocal);
     }
-  }
+  };
 
-  return(
+  return (
     <div className="App">
-      <header>ToDO App</header>
+      <header>ToDo App</header>
 
-      <Form 
-      inputText={inputText}
-      todos={todos} 
-      setTodos={setTodos} 
-      setInputText={setInputText}
-      setStatus={setStatus}/>
+      <Form
+        inputText={inputText}
+        todos={todos}
+        setTodos={setTodos}
+        setInputText={setInputText}
+        setStatus={setStatus}
+      />
 
-      <TodoList 
-      filteredTodos={filteredTodos} 
-      setTodos={setTodos} 
-      todos={todos} />
+      <TodoList
+        filteredTodos={filteredTodos}
+        setTodos={setTodos}
+        todos={todos}
+      />
     </div>
-  )
-}
+  );
+};
 
 export default App;
